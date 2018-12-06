fe107942304354eb45a2467d9364f7562d28792c
ansa1 <abirovansat22@gmail.com>
2018-12-06 05:31:20 +0300

    jar creation added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,35 +3,15 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
-        // Try to find a tree, which can describe input
         try {
-            CSyntaxAnalyzer cSyntaxAnalyzer = new CSyntaxAnalyzer();
-            ParseTree AST = cSyntaxAnalyzer.analyze("./in.txt");
-
-//            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-//
-//            int compilationResult = compiler.run(null, null, null, "CompiledMain");
-//
-//            if (compilationResult == 0) {
-//
-//                System.out.println("Compilation is successful");
-//
-//            } else {
-//                System.out.println("Compilation Failed");
-//            }
-            cSyntaxAnalyzer.serializeTree("./out.txt");
-        } catch (Exception err) {
-            // Else there are errors, which program writes to console
-            System.out.println("An error occured while performing syntax analysis. Please see log.txt");
-            try (PrintWriter out = new PrintWriter("log.txt")) {
-                out.println(err);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+            JarCreator.run();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
